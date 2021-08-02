237cbd52fbbc00400ba2031e1268673c7a34a1b9
John Fitzpatrick <johnfitzpatrick@Johns-MacBook-Pro.local>
2021-08-02 10:58:29 +0100

    Updates

--- check_lab.py
+++ check_lab.py
@@ -4,10 +4,11 @@ import requests
 import json
 import os
 import subprocess
+import tarfile
 from datetime import datetime
 
-now = datetime.now()
-timestamp = datetime.timestamp(now)
+# now = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
+now = datetime.now().strftime("%Y-%m-%d_%I-%M-%S-%p")
 
 passcounter=0
 failcounter=0
@@ -16,36 +17,52 @@ testcounter=0
 testcounter +=1
 url = "http://localhost:8001/services"
 response = requests.request("GET", url)
-if response.json()["data"][0]["name"] == 'mocking_service':
-    passcounter +=1
-else:
+try:
+    if response.json()["data"][0]["name"] == 'mocking_service':
+        passcounter +=1
+    else:
+        failcounter +=1
+        print("'mocking_service' not configured")
+except:
     failcounter +=1
     print("Service not configured")
 
 testcounter +=1
 url = "http://localhost:8001/routes"
 response = requests.request("GET", url)
-if response.json()["data"][0]["name"] == 'mocking':
-    passcounter +=1
-else:
+try:
+    if response.json()["data"][0]["name"] == 'mocking':
+        passcounter +=1
+    else:
+        failcounter +=1
+        print("Route '/mock' not configured")
+except:
     failcounter +=1
     print("Route not configured")
 
 testcounter +=1
 url = "http://localhost:8001/consumers"
 response = requests.request("GET", url)
-if response.json()["data"][0]["username"] == 'Jane':
-    passcounter +=1
-else:
+try:
+    if response.json()["data"][0]["username"] == 'Jane':
+        passcounter +=1
+    else:
+        failcounter +=1
+        print("Consumer 'Jane' not configured")
+except:
     failcounter +=1
     print("Consumer not configured")
 
 testcounter +=1
 url = "http://localhost:8001/plugins"
 response = requests.request("GET", url)
-if response.json()["data"][0]["name"] == 'rate-limiting':
-    passcounter +=1
-else:
+try:
+    if response.json()["data"][0]["name"] == 'rate-limiting':
+        passcounter +=1
+    else:
+        failcounter +=1
+        print("Plugin 'rate-limiting' not configured")
+except:
     failcounter +=1
     print("Plugin not configured")
 
@@ -54,7 +71,6 @@ url = "http://localhost:8000/mock/request"
 response = requests.request("GET", url)
 if response.status_code == 200:
     passcounter +=1
-    print("Proxy request passed")
 else:
     failcounter +=1
     print("Proxy request failed")
@@ -75,17 +91,31 @@ print("Number of tests: " + str(testcounter))
 print("Number of Fails: " + str(failcounter))
 print("Number of Passes: " + str(passcounter))
 
-name = os.environ["AVL_STUDENT_ID"]
-kongfilename = "env_" + name + "_" + str(timestamp) + "_kong.yaml"
-envfilename = "kong_" + name + "_" + str(timestamp) + "_env.out"
-subprocess.call(['deck', 'dump', '-o', kongfilename])
+percent = round((passcounter) * 100) / testcounter
+print("Score: " + str(percent)  + "%")
 
-# env = os.environ
+studentemail = os.environ["AVL_STUDENT_ID"]
+kongfile = "kong_" + studentemail + "_" + str(now) + "_kong.yaml"
+envfile = "env_" + studentemail + "_" + str(now) + "_env.out"
+
+subprocess.call(['deck', 'dump', '-o', kongfile])
+
 for k, v in os.environ.items():
     # print(f'{k}={v}')
-    file = open(envfilename, "a")
+    file = open(envfile, "a")
     file.write(f'{k}={v}')
     file.write("\n")
     file.close
+
+studentname = studentemail.split("@")[0]
+# studentname = studentname.split(".")[0]
+
+tarfilename = "labfiles_" + studentname + "_" + str(now) + ".tar.gz"
+
+tf = tarfile.open(tarfilename, mode="w:gz")
+tf.add(kongfile)
+tf.add(envfile)
+tf.close()
+
 
 # https://www.tutorialspoint.com/python/python_sending_email.htm
--- start_lab_exam.sh
+++ start_lab_exam.sh
@@ -3,4 +3,4 @@
 yes | deck reset > /dev/null
 
 # 20mins is 1200 seconds. Setting to 10 secs for testing
-bash -c 'sleep 10; ./check_lab.sh' &
+bash -c 'sleep 10; ./check_lab.py' &
